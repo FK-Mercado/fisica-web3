1eefeea673454da435b91929031d5e33db1d77ba
martin escobar <martinescob36@gmail.com>
2025-11-06 22:26:48 -0500

    vite.conf4

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // ⚠️ Reemplaza 'nombre-del-repo' por el nombre real de tu repositorio de GitHub
 export default defineConfig({
-    base: './',
+    base: '/fisica-web3',
     build: {
         outDir: 'dist'
     }
