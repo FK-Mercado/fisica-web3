59864249fac36fadafabba29f510b26384fb9259
martin escobar <martinescob36@gmail.com>
2025-11-06 21:54:05 -0500

    vite.conf3

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // ⚠️ Reemplaza 'nombre-del-repo' por el nombre real de tu repositorio de GitHub
 export default defineConfig({
-    base: 'https://fk-mercado.github.io/fisica-web3',
+    base: 'https://github.com/FK-Mercado/fisica-web3',
     build: {
         outDir: 'dist'
     }
